cfdcf0d97cab435362a10d50a0a30e842802bf3e
arakeman <arakeman@outlook.com>
2017-02-18 11:05:52 -0800

    some extra fields

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def webhook():
                     last_name = r.json()["last_name"]
                     if "text" in messaging_event["message"].keys():
                         message_text = messaging_event["message"]["text"]  # the message's text
-                        send_message(sender_id, ("Sorry " + first_name + ", I don't understand \'" + message_text + "\'"))
+                        send_message(sender_id, ("Sorry, I don't understand \'" + message_text + "\'"))
                     elif "attachments" in messaging_event["message"].keys():
                         if "title" in messaging_event["message"]["attachments"][0].keys():
                             title = messaging_event["message"]["attachments"][0]["title"]
