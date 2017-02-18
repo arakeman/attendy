90aa7350da4c8613a20a74e64bab1ad674a8a3ce
arakeman <arakeman@outlook.com>
2017-02-18 10:55:22 -0800

    some extra fields

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def webhook():
                                 lon = coordinates["long"]
                                 time = time.strftime("%H:%M:%S")
                                 date = time.strftime("%d/%m/%Y")
-                                worksheet.append_row([last_name, first_name, title, lat, lon])
+                                worksheet.append_row([date, time, last_name, first_name, title, lat, lon])
                                 send_message(sender_id, ("Thanks " + first_name + ", I have processed your attendance!"))
                             else:
                                 send_message(sender_id, (first_name + ", please send your current location."))
