6ef461365f7f725ea504cd9560f92ab8f5b8c54b
arakeman <arakeman@outlook.com>
2017-02-28 15:16:00 -0800

    LOL

--- quickstart.py
+++ quickstart.py
@@ -1,19 +1,19 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+names = [ "Suhani Abdullah", "KENDRA ABELLANEDA", "Sunny Aggarwal", "JIMI AJAYI", 
+"Yvette Ankunda", "Daksh Bhatia", "Sabrina Cherny", "Aditya Chopra", "Natalie Chow", 
+"Jennifer Dai", "Akshat Das", "KARAN DHILLON", "Charles Ding", "ANTHONY DIPRINZIO", 
+"Liam Ehrlich", "Jon Epstein", "Brennan Fahselt", "ARNAV Gautam", "Mudit Goyal",
+"Tyler Heintz", "Adithya Iyengar", "Brian Jue", "Sung Kang", "Parisa Khorram",
+"Daniela Kim", "Jennifer Kirby", "Ronen Ke", "Rishi Kolady", "Nikhil Krishnan",
+"Anushri Kumar", "Katarina Lee", "FEDERICO LI", "Therese Liwanag", "Riley Shore Mangubat",
+"Sergey Mann", "DARYUS MEDORA", "Brian Mickle", "Brian Nguyen", "Nelli Petikyan",
+"Sanjay Raavi", "Rohit Rajkumar", "Alexander Rakeman", "Evan Sheng", "ROBERT SPRAGG",
+"Chase Sturgill", "Iris Sun", "KESHAV THVAR", "Nicole Tsai", "Jessie Wang",
+"William Wang", "Thomas Warloe", "Serena Wu", "Kenny Yoo", "Sunny Zhang", "Zhe Zhang" ]
 
-scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/drive'
-]
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name('My Project-3aa3cf8e8cee.json', scope)
-
-gc = gspread.authorize(credentials)
-
-sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1ghA2W0tGvK0HSb55eER7UVBHwdjW5WUgcAaNYHmpy1E/')
-
-worksheet = sh.get_worksheet(0)
-
-values_list = worksheet.col_values(1)
-
-print(values_list)
+counter = 2
+print("{")
+for name in names:
+	for i in range(1, 4):
+		print("\"" + name.lower() + " " + str(i) + "\" : " + str(counter) + ",")
+		counter = counter + 1
+print("}")
